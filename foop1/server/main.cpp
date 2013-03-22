aa83c87d444bb657926dffc0334a1a08c2baece9
Jakob Gruber <jakob.gruber@gmail.com>
2013-03-22 16:06:10 +0100

    Set up server logging

--- foop1/server/main.cpp
+++ foop1/server/main.cpp
@@ -1,8 +1,18 @@
 #include <QCoreApplication>
 
+#include "QsLog.h"
+#include "QsLogDest.h"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    QsLogging::Logger &logger = QsLogging::Logger::instance();
+    logger.setLoggingLevel(QsLogging::TraceLevel);
+
+    QsLogging::DestinationPtr debugDestination(
+        QsLogging::DestinationFactory::MakeDebugOutputDestination());
+    logger.addDestination(debugDestination.get());
 
     return a.exec();
 }
