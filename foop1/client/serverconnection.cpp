226aa78d06b23b8a6a509d2fb39b9e97a8d87379
Jakob Gruber <jakob.gruber@gmail.com>
2013-03-25 09:38:40 +0100

    Initialize the server connection socket

--- foop1/client/serverconnection.cpp
+++ foop1/client/serverconnection.cpp
@@ -1,5 +1,7 @@
 #include "serverconnection.h"
 
+#include <QTcpSocket>
+
 #include "jsonvariantsocket.h"
 #include "QsLog.h"
 
@@ -13,4 +15,14 @@ ServerConnection::ServerConnection(QString host, int port, QObject *parent) :
 void ServerConnection::run()
 {
     QLOG_TRACE() << __PRETTY_FUNCTION__;
+
+    QSharedPointer<QTcpSocket> tcpSocket(new QTcpSocket());
+    tcpSocket->connectToHost(host, port);
+
+    if (!tcpSocket->waitForConnected()) {
+        QLOG_ERROR() << tcpSocket->errorString();
+        emit finished();
+    }
+
+    variantSocket.reset(new JsonVariantSocket(tcpSocket));
 }
