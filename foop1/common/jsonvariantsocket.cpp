1505c28ada37c83033e7293dec0fdf4441b52db1
Jakob Gruber <jakob.gruber@gmail.com>
2013-04-05 15:51:20 +0200

    Ensure the parser only ever receives a single complete message

--- foop1/common/jsonvariantsocket.cpp
+++ foop1/common/jsonvariantsocket.cpp
@@ -2,6 +2,8 @@
 
 #include "QsLog.h"
 
+#define EOM ("#")
+
 JsonVariantSocket::JsonVariantSocket(QSharedPointer<QTcpSocket> tcpSocket, QObject *parent) :
     AbstractVariantSocket(parent),
     tcpSocket(tcpSocket)
@@ -29,22 +31,38 @@ void JsonVariantSocket::write(const QVariant &data)
     QByteArray bytes = serializer->serialize(data);
 
     tcpSocket->write(bytes);
+    tcpSocket->write(EOM);
 }
 
 void JsonVariantSocket::onReadyRead()
 {
     QLOG_TRACE() << __PRETTY_FUNCTION__;
 
+    /* The parser implementation is really picky. It expects exactly one
+     * well-formed JSON object; if it ever fails, it never recovers
+     * correctly. Thus, we need to ensure that only correct objects are
+     * ever delivered to it. We do this by inserting an 'end-of-message'
+     * character after each message. Of course this assumes that no valid
+     * message contains this character. */
+
     parseBuffer.append(tcpSocket->readAll());
 
-    if (!isJsonOk(parseBuffer)) {
+    int eomIndex = parseBuffer.indexOf(EOM);
+    if (eomIndex == -1) {
         return;
     }
 
+    /* We've received a complete message. Attempt to parse the message
+     * and remove it from the incoming data buffer. */
+
+    QByteArray msgBuffer = parseBuffer.left(eomIndex);
+    parseBuffer = parseBuffer.remove(0, eomIndex + 1);
+
     bool ok;
-    QVariant result = parser->parse(parseBuffer, &ok);
+    QVariant result = parser->parse(msgBuffer, &ok);
 
     if (!ok) {
+        QLOG_ERROR() << "Failed to parse incoming message:" << msgBuffer;
         return;
     }
 
@@ -52,7 +70,6 @@ void JsonVariantSocket::onReadyRead()
      * the incoming data. */
 
     QLOG_TRACE() << "Received JSON:" << result;
-    parseBuffer.clear();
 
     readQueue.append(result);
     emit readyRead();
