480cc7045260c7ff4ab010a363b29933bae16349
Jakob Gruber <jakob.gruber@gmail.com>
2013-04-09 14:24:07 +0200

    Remove dummy snakes from Board
    Snakes are now added when players join.

--- foop1/common/board.cpp
+++ foop1/common/board.cpp
@@ -4,11 +4,6 @@ Board::Board(int width, int height) :
     width(width),
     height(height)
 {
-    /* TODO: Temp. */
-
-    snakes.append(QSharedPointer<Snake>(new Snake(0)));
-    snakes.append(QSharedPointer<Snake>(new Snake(1)));
-    snakes.append(QSharedPointer<Snake>(new Snake(2)));
 }
 
 int Board::getWidth() const
