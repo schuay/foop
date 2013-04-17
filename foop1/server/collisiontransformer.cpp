eedec5be0a745522a8bf582c05ec6518ab0bb98b
Jakob Gruber <jakob.gruber@gmail.com>
2013-04-17 17:39:33 +0200

    Refactor CollisionTransformer

--- foop1/server/collisiontransformer.cpp
+++ foop1/server/collisiontransformer.cpp
@@ -11,6 +11,100 @@ struct PartialRemoval {
     int collisionIndex;
 };
 
+void handleCollisionWithSelf(const QSharedPointer<Snake> &snake,
+                             QList<QSharedPointer<Snake> > &toRemove)
+{
+    QList<QPoint> body = snake->getBody();
+    QPoint head = body.last();
+    body.pop_back();
+
+    /* Check if a snake's head collides with itself. If it does,
+     * that snake has lost and is removed from the board. */
+
+    foreach(const QPoint & p, body) {
+        if (p == head) {
+            QLOG_DEBUG() << "Detected collision with self";
+
+            toRemove.append(snake);
+
+            /* TODO: Send game over message. */
+        }
+    }
+}
+
+void handleCollisionWithOtherHead(const QSharedPointer<Snake> &snake,
+                                  const QSharedPointer<Snake> &otherSnake,
+                                  QList<QSharedPointer<Snake> > &toRemove)
+{
+    QPoint head = snake->getBody().last();
+
+    QList<QPoint> otherBody = otherSnake->getBody();
+    QPoint otherHead = otherBody.last();
+    otherBody.pop_back();
+
+    /* If it collides with another snake's head, the snake with the
+     * higher priority wins. If priorities are equal, each snake
+     * has a 50% chance to win. */
+
+    if (head == otherHead) {
+        QLOG_DEBUG() << "Detected collision with other head";
+
+        QSharedPointer<Snake> winner, loser;
+        bool iWin = (qrand() % 2) == 0;
+
+        Snake::Priority thisPriority = snake->getPriority();
+        Snake::Priority thatPriority = otherSnake->getPriority();
+
+        if (thisPriority > thatPriority || (thisPriority == thatPriority && iWin)) {
+            winner = snake;
+            loser = otherSnake;
+        } else {
+            winner = otherSnake;
+            loser = snake;
+        }
+
+        winner->setPendingGrowth(winner->getPendingGrowth() + loser->getBody().size());
+        toRemove.append(loser);
+
+        /* TODO: Send game over message. */
+    }
+}
+
+void handleCollisionWithOtherBody(const QSharedPointer<Snake> &snake,
+                                  const QSharedPointer<Snake> &otherSnake,
+                                  QList<PartialRemoval> &toPartialRemove)
+{
+    QPoint head = snake->getBody().last();
+
+    QList<QPoint> otherBody = otherSnake->getBody();
+    otherBody.pop_back();
+
+    /* If it collides with another snake's body, the other snake is
+     * *partially* eaten. */
+
+    for (int i = 0; i < otherBody.size(); i++) {
+        QPoint p = otherBody[i];
+
+        if (p != head) {
+            continue;
+        }
+
+        PartialRemoval partialRemoval;
+        partialRemoval.snake = otherSnake;
+        partialRemoval.collisionIndex = i + 1; /* Plus one because we've removed head. */
+
+        int removedElements = otherBody.size() - i;
+        snake->setPendingGrowth(snake->getPendingGrowth() + removedElements);
+
+        QLOG_DEBUG() << "Detected collision with other body; gained"
+                     << removedElements << "elements";
+
+        toPartialRemove.append(partialRemoval);
+
+        /* TODO: Game over message. */
+    }
+}
+
 CollisionTransformer::CollisionTransformer()
 {
     qsrand(QTime::currentTime().msec());
@@ -29,23 +123,9 @@ void CollisionTransformer::transform(Game *game)
 
     QSharedPointer<Board> board = game->getBoard();
     foreach(const QSharedPointer<Snake> &snake, board->getSnakes()) {
-        QList<QPoint> body = snake->getBody();
-        QPoint head = body.last();
-        body.pop_back();
+        handleCollisionWithSelf(snake, toRemove);
 
-        /* Check if a snake's head collides with itself. If it does,
-         * that snake has lost and is removed from the board. */
-
-        foreach(const QPoint & p, body) {
-            if (p == head) {
-                QLOG_DEBUG() << "Detected collision with self";
-
-                toRemove.append(snake);
-
-                /* TODO: Send game over message. */
-            }
-        }
-
+        QPoint head = snake->getBody().last();
         foreach(const QSharedPointer<Snake> &otherSnake, board->getSnakes()) {
             /* Skip self. */
 
@@ -53,62 +133,8 @@ void CollisionTransformer::transform(Game *game)
                 continue;
             }
 
-            QList<QPoint> otherBody = otherSnake->getBody();
-            QPoint otherHead = otherBody.last();
-            otherBody.pop_back();
-
-            /* If it collides with another snake's head, the snake with the
-             * higher priority wins. If priorities are equal, each snake
-             * has a 50% chance to win. */
-
-            if (head == otherHead) {
-                QLOG_DEBUG() << "Detected collision with other head";
-
-                QSharedPointer<Snake> winner, loser;
-                bool iWin = (qrand() % 2) == 0;
-
-                Snake::Priority thisPriority = snake->getPriority();
-                Snake::Priority thatPriority = otherSnake->getPriority();
-
-                if (thisPriority > thatPriority || (thisPriority == thatPriority && iWin)) {
-                    winner = snake;
-                    loser = otherSnake;
-                } else {
-                    winner = otherSnake;
-                    loser = snake;
-                }
-
-                winner->setPendingGrowth(winner->getPendingGrowth() + loser->getBody().size());
-                toRemove.append(loser);
-
-                /* TODO: Send game over message. */
-
-                continue;
-            }
-
-            /* If it collides with another snake's body, the other snake is
-             * *partially* eaten. */
-
-            for (int i = 0; i < otherBody.size(); i++) {
-                QPoint p = otherBody[i];
-
-                if (p != head) {
-                    continue;
-                }
-
-                QLOG_DEBUG() << "Detected collision with other body";
-
-                PartialRemoval partialRemoval;
-                partialRemoval.snake = otherSnake;
-                partialRemoval.collisionIndex = i + 1; /* Plus one because we've removed head. */
-
-                int removedElements = otherBody.size() - i;
-                snake->setPendingGrowth(snake->getPendingGrowth() + removedElements);
-
-                toPartialRemove.append(partialRemoval);
-
-                /* TODO: Game over message. */
-            }
+            handleCollisionWithOtherHead(snake, otherSnake, toRemove);
+            handleCollisionWithOtherBody(snake, otherSnake, toPartialRemove);
         }
     }
 
